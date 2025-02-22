d578d7114ff2466ea21e78198eafd838710bcfa6
KoshTatus <153766064+KoshTatus@users.noreply.github.com>
2025-02-22 18:41:26 +0400

    style: change design on detail screen

--- app/src/main/java/com/example/mycityapp/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/example/mycityapp/ui/screens/DetailScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.mycityapp.R
@@ -64,7 +65,7 @@ fun DetailScreen(
             )
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier.fillMaxSize().padding(16.dp),
             ) {
                 Text(
                     text = place.value?.description ?: "",
@@ -74,6 +75,7 @@ fun DetailScreen(
                 Text(
                     text = place.value?.address ?: "",
                     style = MaterialTheme.typography.titleLarge,
+                    textDecoration = TextDecoration.Underline,
                     modifier = Modifier.padding(20.dp),
                     textAlign = TextAlign.Center
                 )
